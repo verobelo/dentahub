9c947ae2d3ff396f1fb86741899fa4430ca22c52
verobelo <142001535+verobelo@users.noreply.github.com>
2025-09-29 16:49:28 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next";
-import { Nunito_Sans } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from 'next';
+import { Nunito_Sans } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const nunito = Nunito_Sans({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
-  title: "DentaHub",
+  title: 'DentaHub',
   description:
-    "Simplify dental care management with DentaHub. Patients easily schedule appointments and view treatment history. Dental professionals manage patient schedules, track procedures, and streamline practice operations all in one platform.",
+    'Simplify dental care management with DentaHub. Patients easily schedule appointments and view treatment history. Dental professionals manage patient schedules, track procedures, and streamline practice operations all in one platform.',
 };
 
 export default function RootLayout({
@@ -21,11 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body
-        className={cn("bg-dark-300 min-h-screen antialiased", nunito.className)}
-      >
-        <ThemeProvider attribute="class" defaultTheme="system">
+        className={cn(
+          'bg-dark-300 min-h-screen antialiased',
+          nunito.className
+        )}>
+        <ThemeProvider attribute='class' defaultTheme='dark'>
           {children}
         </ThemeProvider>
       </body>
