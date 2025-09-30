0bc3fe77095681d3179b08b8c1ad3e44287af890
verobelo <142001535+verobelo@users.noreply.github.com>
2025-09-30 19:13:41 +0200

    Fix img url

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -52,7 +52,7 @@ export async function registerPatient({
     data: {
       identificationDocumentId: file?.$id ? file.$id : null,
       identificationDocumentUrl: file?.$id
-        ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view?project=${PROJECT_ID}`
+        ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
         : null,
       ...patient,
     },
